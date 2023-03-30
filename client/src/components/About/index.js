2ff36e311397b0761dc343457bc0256c3bebc37a
elkirko <kirkhagglund@gmail.com>
2023-03-30 09:52:33 -0600

    Added styling to navbar

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -6,7 +6,7 @@ function About() {
     <div id="about-body">
       <h2 className="text-center">Welcome to Chad GPT, your at home virtual personal trainer. </h2>
       <br />
-      <h4 className='text-center pb-5'>
+      <h4 className='text-center pb-5 bg-warning' id="about-background">
         Our mission is to provide personalized workout routines curated for your
         needs and life. Not everyone has the time or money to spend on a fitness
         coach, but the benefits are undeniable. This application is designed to
