d02234cdc2ae5caca1f465f657e025b4b530ed0b
Stefan Marinov <stefan_marinov@rogers.com>
2023-03-31 09:37:11 -0400

    google image custom search

--- client/src/pages/GoogleImages.js
+++ client/src/pages/GoogleImages.js
@@ -1,59 +1,34 @@
 import React from "react";
-import { useState } from "react";
-import GoogleImages from "googleimg"
+import { useState, useEffect } from "react";
+import {searchGoogleImages} from '../utils/API'
 
 
-// client.search('Steve Angello')
-//  .then(images => {
-//   /*
-//   [{
-//     "link": 'http://steveangello.com/boss.jpg',
-//     "mime": 'image/jpeg',
-//     "snippet": 'Steve Angello',
-//     "image": {
-//       "contextLink": 'http://steveangello.com',
-//       "width": 1024,
-//       "height": 768,
-//       "byteSize": 1000,
-//       "thumbnailLink": 'http://steveangello.com/thumbboss.jpg',
-//       "thumbnailWidth": 512,
-//       "thumbnailHeight": 512
-//     }
-//   }]
-//   */
-//  });
+const Home = () => {
 
-// paginate results
-// client.search('Steve Angello', {page: 2});
+  const [woImages, setWOImages] = useState('');
+  const handleEvent = async (event) => {
+    event.preventDefault();
+    try {
+      const results = await searchGoogleImages(event.target.value);
+      const images = await results.json();
+      console.log(images.items[0].link)
+      setWOImages(images.items[0].link);
+      console.log(woImages)
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
-// search for certain size
-// client.search('Steve Angello', {size: 'large'});
-
-
-const Home = async () => {
-const client = new GoogleImages('b465a8304d3e04992', 'AIzaSyBf4eZJ_KIHX7_Z07UM516NrBeABQYPU4s');
-//const [images, setImages] = useState([]);
-//client.search('exercise diagram squat ', {page: 1})
-// .then(data => setImages(data))
-const exercise = "squat"
-const API = `https://cse.google.com/cse?cx=65ded13daa12f421d#gsc.tab=0&gsc.q=${exercise}`
-
-try {
-const results = await fetch(API, {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-})
-const images = await results.json();
-} catch (err) {
-    console.log(err)
-}
 
   return (
     <div className="container">
-        
+      <form >
+        <input type="submit" value="Walking Lunges " onClick={handleEvent} />
+        <input type="submit" value="Treadmill " onClick={handleEvent} />
+        <input type="submit" value="Barbell Squats " onClick={handleEvent} />
+      </form>
+      <img width="50%" src={woImages} alt='diagram' />
+
     </div>
   );
 };
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,11 +1,11 @@
-// import { config } from "dotenv";
-// config();
+require('dotenv').config()
 
-// import { OpenAIApi, Configuration } from "openai";
-
-// const openai = new OpenAIApi(
-//   new Configuration({
-//     apiKey: process.env.API_KEY,
-//   })
-// );
-
+export const searchGoogleImages = (query) => {
+    return fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_GOOGLE_API}&cx=${process.env.REACT_APP_GOOGLE_CX}&q=${query}&num=1&searchType=image&siteSearch=workoutlabs.com&siteSearchFilter=i`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+  };
+  
