1e07d1a1d909b52189fa1faeefd876f1615bfd4a
Stefan Marinov <stefan_marinov@rogers.com>
2023-03-31 11:45:38 -0400

    cleaned up code from old app; updated CSS for footer to always appear at the bottom

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,6 @@ import NoMatch from './pages/NoMatch';
 import { Provider } from 'react-redux';
 import store from './utils/store';
 import MainLanding from './pages/MainLanding';
-//import Dashboard from './pages/Dashboard';
 import BuildWorkout from './pages/BuildWorkout';
 import Footer from './components/Footer';
 import GoogleImages from './pages/GoogleImages'
@@ -39,35 +38,20 @@ const client = new ApolloClient({
 function App() {
   return (
     <Provider store={store}>
-      
-    <ApolloProvider  client={client}>
-   
+      <ApolloProvider client={client}>
         <Router>
           <div>
             <Switch>
-
-              {/*<Route exact path="/" component={Home} />*/}
-              {/*<Route exact path="/login" component={Login} />*/}
-              {/*<Route exact path="/signup" component={Signup} />*/}
-              {/*<Route exact path="/success" component={Success} />*/}
-              {/*<Route exact path="/orderHistory" component={OrderHistory} />*/}
-              {/*<Route exact path="/products/:id" component={Detail} />*/}
               <Route exact path='/' component={MainLanding} /> {/* This will eventually be our default route */}
-              {/* <Route exact path='/dashboard' component={Dashboard} /> This will be contain navbar, accessed upon login */}
               <Route exact path='/buildWorkout' component={BuildWorkout} /> {/* This will be accessed through nav in dashboard */}
-              <Route exact path='/googleImages' component={GoogleImages} /> {/* This will be <Route component={NoMatch} /> */}
-
-              <Route exact path='/' component={MainLanding} /> 
+              <Route exact path='/googleImages' component={GoogleImages} /> {/* This will be sample component until we retrive images in the necessary area*/}
               <Route exact path='/buildWorkout' component={BuildWorkout} />
-              <Route component={NoMatch} />
-
+              <Route path='*' component={NoMatch} />
             </Switch>
             <Footer />
-
           </div>
         </Router>
-
-    </ApolloProvider>
+      </ApolloProvider>
     </Provider>
   );
 }
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,52 +1,5 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_PRODUCTS = gql`
-  query getProducts($category: ID) {
-    products(category: $category) {
-      _id
-      name
-      description
-      price
-      quantity
-      image
-      category {
-        _id
-      }
-    }
-  }
-`;
-
-export const QUERY_CHECKOUT = gql`
-  query getCheckout($products: [ID]!) {
-    checkout(products: $products) {
-      session
-    }
-  }
-`;
-
-export const QUERY_ALL_PRODUCTS = gql`
-  {
-    products {
-      _id
-      name
-      description
-      price
-      quantity
-      category {
-        name
-      }
-    }
-  }
-`;
-
-export const QUERY_CATEGORIES = gql`
-  {
-    categories {
-      _id
-      name
-    }
-  }
-`;
 export const QUERY_SETTINGS = gql`
   query Setting($key: String) {
     setting(key: $key) {
@@ -55,6 +8,7 @@ export const QUERY_SETTINGS = gql`
     }
   }
 `;
+
 export const QUERY_USER = gql`
   {
   user {
