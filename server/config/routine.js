2d84a3601de5264bff6f6f66d80d5e230ee4d683
Stefan Marinov <stefan_marinov@rogers.com>
2023-03-31 13:33:18 -0400

    clean code for the back end

--- server/config/routine.js
+++ server/config/routine.js
@@ -3,127 +3,135 @@
         "day": "Monday",
         "exercises": [
             {
-                "exerciseName": "Treadmill",
-                "exerciseType": "Cardiovascular",
-                "sets": 1,
-                "reps": null,
-                "secondsRest": null,
-                "minutesDuration": 20,
-                "intensity": "Moderate"
+                "exerciseName": "Dynamic Warm-up",
+                "exerciseType": "Bodyweight",
+                "sets": "3",
+                "reps": "10",
+                "secondsRest": "30",
+                "minutesDuration": "0.2",
+                "intensity": "Low"
             },
             {
-                "exerciseName": "Barbell Squats",
-                "exerciseType": "Strength Training",
-                "sets": 3,
-                "reps": 12,
-                "secondsRest": 60,
-                "minutesDuration": null,
+                "exerciseName": "Hip Flexor Stretch",
+                "exerciseType": "Stretching",
+                "sets": "3",
+                "reps": "30 seconds",
+                "secondsRest": "30",
+                "minutesDuration": "0.2",
+                "intensity": "Low"
+            },
+            {
+                "exerciseName": "Side Lunge",
+                "exerciseType": "Bodyweight",
+                "sets": "3",
+                "reps": "10",
+                "secondsRest": "30",
+                "minutesDuration": "0.2",
+                "intensity": "Low"
+            },
+            {
+                "exerciseName": "Leg Extension",
+                "exerciseType": "Machine",
+                "sets": "3",
+                "reps": "12",
+                "secondsRest": "60",
+                "minutesDuration": "0.2",
                 "intensity": "Medium"
             },
             {
-                "exerciseName": "Walking Lunges",
-                "exerciseType": "Strength Training",
-                "sets": 3,
-                "reps": 12,
-                "secondsRest": 60,
-                "minutesDuration": null,
-                "intensity": "High"
+                "exerciseName": "Seated Hamstring Curl",
+                "exerciseType": "Machine",
+                "sets": "3",
+                "reps": "12",
+                "secondsRest": "60",
+                "minutesDuration": "0.2",
+                "intensity": "Medium"
+            },
+            {
+                "exerciseName": "Calf Raise",
+                "exerciseType": "Machine",
+                "sets": "3",
+                "reps": "15",
+                "secondsRest": "60",
+                "minutesDuration": "0.2",
+                "intensity": "Medium"
+            },
+            {
+                "exerciseName": "Static Stretching",
+                "exerciseType": "Stretching",
+                "sets": "3",
+                "reps": "30 seconds",
+                "secondsRest": "30",
+                "minutesDuration": "0.2",
+                "intensity": "Low"
             }
         ]
     },
     {
-        "day": "Tuesday",
+        "day": "Wednesday",
         "exercises": [
             {
-                "exerciseName": "Treadmill",
-                "exerciseType": "Cardiovascular",
-                "sets": 1,
-                "reps": null,
-                "secondsRest": null,
-                "minutesDuration": 20,
-                "intensity": "Moderate"
+                "exerciseName": "Dynamic Warm-up",
+                "exerciseType": "Bodyweight",
+                "sets": "3",
+                "reps": "10",
+                "secondsRest": "30",
+                "minutesDuration": "0.2",
+                "intensity": "Low"
             },
             {
-                "exerciseName": "Barbell Squats",
-                "exerciseType": "Strength Training",
-                "sets": 3,
-                "reps": 12,
-                "secondsRest": 60,
-                "minutesDuration": null,
+                "exerciseName": "Hip Flexor Stretch",
+                "exerciseType": "Stretching",
+                "sets": "3",
+                "reps": "30 seconds",
+                "secondsRest": "30",
+                "minutesDuration": "0.2",
+                "intensity": "Low"
+            },
+            {
+                "exerciseName": "Bodyweight Squat",
+                "exerciseType": "Bodyweight",
+                "sets": "3",
+                "reps": "15",
+                "secondsRest": "90",
+                "minutesDuration": "0.2",
                 "intensity": "Medium"
             },
             {
-                "exerciseName": "Walking Lunges",
-                "exerciseType": "Strength Training",
-                "sets": 3,
-                "reps": 12,
-                "secondsRest": 60,
-                "minutesDuration": null,
-                "intensity": "High"
-            }
-        ]
-    },
-    {
-        "day": "Wednesay",
-        "exercises": [
-            {
-                "exerciseName": "Treadmill",
-                "exerciseType": "Cardiovascular",
-                "sets": 1,
-                "reps": null,
-                "secondsRest": null,
-                "minutesDuration": 20,
-                "intensity": "Moderate"
-            },
-            {
-                "exerciseName": "Barbell Squats",
-                "exerciseType": "Strength Training",
-                "sets": 3,
-                "reps": 12,
-                "secondsRest": 60,
-                "minutesDuration": null,
+                "exerciseName": "Step-Up",
+                "exerciseType": "Bodyweight",
+                "sets": "3",
+                "reps": "12",
+                "secondsRest": "60",
+                "minutesDuration": "0.2",
                 "intensity": "Medium"
             },
             {
-                "exerciseName": "Walking Lunges",
-                "exerciseType": "Strength Training",
-                "sets": 3,
-                "reps": 12,
-                "secondsRest": 60,
-                "minutesDuration": null,
-                "intensity": "High"
-            }
-        ]
-    },
-    {
-        "day": "Thursday",
-        "exercises": [
-            {
-                "exerciseName": "Treadmill",
-                "exerciseType": "Cardiovascular",
-                "sets": 1,
-                "reps": null,
-                "secondsRest": null,
-                "minutesDuration": 20,
-                "intensity": "Moderate"
-            },
-            {
-                "exerciseName": "Barbell Squats",
-                "exerciseType": "Strength Training",
-                "sets": 3,
-                "reps": 12,
-                "secondsRest": 60,
-                "minutesDuration": null,
+                "exerciseName": "Lunges",
+                "exerciseType": "Bodyweight",
+                "sets": "3",
+                "reps": "10",
+                "secondsRest": "60",
+                "minutesDuration": "0.2",
                 "intensity": "Medium"
             },
             {
-                "exerciseName": "Walking Lunges",
-                "exerciseType": "Strength Training",
-                "sets": 3,
-                "reps": 12,
-                "secondsRest": 60,
-                "minutesDuration": null,
-                "intensity": "High"
+                "exerciseName": "Calf Raise",
+                "exerciseType": "Machine",
+                "sets": "3",
+                "reps": "15",
+                "secondsRest": "60",
+                "minutesDuration": "0.2",
+                "intensity": "Medium"
+            },
+            {
+                "exerciseName": "Static Stretching",
+                "exerciseType": "Stretching",
+                "sets": "3",
+                "reps": "30 seconds",
+                "secondsRest": "30",
+                "minutesDuration": "0.2",
+                "intensity": "Low"
             }
         ]
     },
@@ -131,98 +139,70 @@
         "day": "Friday",
         "exercises": [
             {
-                "exerciseName": "Treadmill",
-                "exerciseType": "Cardiovascular",
-                "sets": 1,
-                "reps": null,
-                "secondsRest": null,
-                "minutesDuration": 20,
-                "intensity": "Moderate"
+                "exerciseName": "Dynamic Warm-up",
+                "exerciseType": "Bodyweight",
+                "sets": "3",
+                "reps": "10",
+                "secondsRest": "30",
+                "minutesDuration": "0.2",
+                "intensity": "Low"
             },
             {
-                "exerciseName": "Barbell Squats",
-                "exerciseType": "Strength Training",
-                "sets": 3,
-                "reps": 12,
-                "secondsRest": 60,
-                "minutesDuration": null,
+                "exerciseName": "Hip Flexor Stretch",
+                "exerciseType": "Stretching",
+                "sets": "3",
+                "reps": "30 seconds",
+                "secondsRest": "30",
+                "minutesDuration": "0.2",
+                "intensity": "Low"
+            },
+            {
+                "exerciseName": "Deadlift",
+                "exerciseType": "Weight Training",
+                "sets": "3",
+                "reps": "10",
+                "secondsRest": "60",
+                "minutesDuration": "0.2",
                 "intensity": "Medium"
             },
             {
-                "exerciseName": "Walking Lunges",
-                "exerciseType": "Strength Training",
-                "sets": 3,
-                "reps": 12,
-                "secondsRest": 60,
-                "minutesDuration": null,
-                "intensity": "High"
-            }
-        ]
-    },
-    {
-        "day": "SAturday",
-        "exercises": [
-            {
-                "exerciseName": "Treadmill",
-                "exerciseType": "Cardiovascular",
-                "sets": 1,
-                "reps": null,
-                "secondsRest": null,
-                "minutesDuration": 20,
-                "intensity": "Moderate"
-            },
-            {
-                "exerciseName": "Barbell Squats",
-                "exerciseType": "Strength Training",
-                "sets": 3,
-                "reps": 12,
-                "secondsRest": 60,
-                "minutesDuration": null,
+                "exerciseName": "Standing Calf Raise",
+                "exerciseType": "Machine",
+                "sets": "3",
+                "reps": "15",
+                "secondsRest": "60",
+                "minutesDuration": "0.2",
                 "intensity": "Medium"
             },
             {
-                "exerciseName": "Walking Lunges",
-                "exerciseType": "Strength Training",
-                "sets": 3,
-                "reps": 12,
-                "secondsRest": 60,
-                "minutesDuration": null,
-                "intensity": "High"
-            }
-        ]
-    },
-    {
-        "day": "Sunday",
-        "exercises": [
-            {
-                "exerciseName": "Treadmill",
-                "exerciseType": "Cardiovascular",
-                "sets": 1,
-                "reps": null,
-                "secondsRest": null,
-                "minutesDuration": 20,
-                "intensity": "Moderate"
-            },
-            {
-                "exerciseName": "Barbell Squats",
-                "exerciseType": "Strength Training",
-                "sets": 3,
-                "reps": 12,
-                "secondsRest": 60,
-                "minutesDuration": null,
+                "exerciseName": "Hamstring Curl",
+                "exerciseType": "Machine",
+                "sets": "3",
+                "reps": "12",
+                "secondsRest": "60",
+                "minutesDuration": "0.2",
                 "intensity": "Medium"
             },
             {
-                "exerciseName": "Walking Lunges",
-                "exerciseType": "Strength Training",
-                "sets": 3,
-                "reps": 12,
-                "secondsRest": 60,
-                "minutesDuration": null,
-                "intensity": "High"
+                "exerciseName": "Glute Bridge",
+                "exerciseType": "Bodyweight",
+                "sets": "3",
+                "reps": "15",
+                "secondsRest": "60",
+                "minutesDuration": "0.2",
+                "intensity": "Medium"
+            },
+            {
+                "exerciseName": "Static Stretching",
+                "exerciseType": "Stretching",
+                "sets": "3",
+                "reps": "30 seconds",
+                "secondsRest": "30",
+                "minutesDuration": "0.2",
+                "intensity": "Low"
             }
         ]
     }
-];
+]
 
 module.exports = exerciseRoutine;
