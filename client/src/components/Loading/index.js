bd3485538f569fe4b6c5614a20ef8f4ad166f1b4
Mario Repas <mario.repas63@gmail.com>
2023-04-01 15:44:59 -0400

    DEBUG: Loading page now cycles through all quotes

--- client/src/components/Loading/index.js
+++ client/src/components/Loading/index.js
@@ -1,21 +1,22 @@
 import React, { useState, useEffect } from "react";
 
+const exerciseQuotes = [
+  "A one-hour workout is 4% of your day. No excuses.",
+  "Fitness is not about being better than someone else. It's about being better than you used to be.",
+  "The only bad workout is the one that didn't happen.",
+  "Sweat is just fat crying.",
+  "The difference between try and triumph is just a little umph!",
+  "Your body can stand almost anything. It's your mind that you have to convince.",
+  "The only way to finish is to start.",
+  "Strive for progress, not perfection.",
+  "The pain you feel today will be the strength you feel tomorrow.",
+  "It's not about having time. It's about making time.",
+];
+
 function Loading() {
   const [quote, setQuote] = useState("");
 
-  const exerciseQuotes = [
-    "A one-hour workout is 4% of your day. No excuses.",
-    "Fitness is not about being better than someone else. It's about being better than you used to be.",
-    "The only bad workout is the one that didn't happen.",
-    "Sweat is just fat crying.",
-    "The difference between try and triumph is just a little umph!",
-    "Your body can stand almost anything. It's your mind that you have to convince.",
-    "The only way to finish is to start.",
-    "Strive for progress, not perfection.",
-    "The pain you feel today will be the strength you feel tomorrow.",
-    "It's not about having time. It's about making time.",
-  ];
-
+// useEffect hook is used to run the code inside the callback function only once when the component is mounted
   useEffect(() => {
     let index = 0;
     const intervalId = setInterval(() => {
@@ -23,7 +24,7 @@ function Loading() {
       index = (index + 1) % exerciseQuotes.length;
     }, 5000);
     return () => clearInterval(intervalId);
-  }, [exerciseQuotes]);
+  }, []); // Updated the dependency array to an empty array
 
   return (
     <div>
