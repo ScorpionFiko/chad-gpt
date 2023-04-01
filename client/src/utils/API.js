b5a6f76a34f725e235eb76e47d9ae3da16ba5d2e
Stefan Marinov <stefan_marinov@rogers.com>
2023-04-01 10:41:15 -0400

    sample code for testing access to env variables

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,14 @@
 require('dotenv').config()
 
-export const searchGoogleImages = (query) => {
+export const searchGoogleImages = async (query) => {
+
+    const test = await fetch("/config", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const config = await test.json();
     return fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_GOOGLE_API}&cx=${process.env.REACT_APP_GOOGLE_CX}&q=${query}&num=1&searchType=image&siteSearch=workoutlabs.com&siteSearchFilter=i`, {
         method: "GET",
         headers: {
--- server/server.js
+++ server/server.js
@@ -26,6 +26,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('/config', (req, res) => {
+  res.json(process.env.GOOGLE_CX);
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
