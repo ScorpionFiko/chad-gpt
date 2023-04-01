17a604e64aec39d9ac45fe6244c3e9ca7ce032d7
Mario Repas <mario.repas63@gmail.com>
2023-04-01 19:56:36 -0400

    STYLING: Navbar expands/collapses into hamburger

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,14 +1,33 @@
-import React from "react";
-import Auth from "../../utils/auth";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import Auth from '../../utils/auth';
+import { Link } from 'react-router-dom';
+import './style.css';
 
 function Navigation() {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => {
+    setCollapsed(!collapsed);
+  };
+
+  const closeNavbar = () => {
+    if (!collapsed) setCollapsed(true);
+  };
+
+  const navClass = collapsed ? 'collapse navbar-collapse' : 'navbar-collapse';
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg custom-navbar">
       <div className="navbar-brand px-3">Chad GPT</div>
-      <div id="navbarNav">
-        <ul className="navbar-nav px-2">
-          {" "}
+      <button
+        className="navbar-toggler"
+        type="button"
+        onClick={toggleNavbar}
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div id="navbarNav" className={navClass}>
+        <ul className="navbar-nav px-2" onClick={closeNavbar}>
           <li className="nav-item active">
             <Link className="nav-link" to="/">
               My Dashboard
@@ -20,7 +39,14 @@ function Navigation() {
             </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/" onClick={() => Auth.logout()}>
+            <a
+              className="nav-link"
+              href="/"
+              onClick={(e) => {
+                e.preventDefault();
+                Auth.logout();
+              }}
+            >
               Logout
             </a>
           </li>
