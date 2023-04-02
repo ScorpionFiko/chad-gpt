deee2dd0405c8741c76806168cce15e1ef3c34e0
elkirko <kirkhagglund@gmail.com>
2023-04-02 15:04:10 -0600

    Added conditional rendering to details component

--- client/src/components/WorkoutDetails/index.js
+++ client/src/components/WorkoutDetails/index.js
@@ -1,20 +1,44 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
-function WorkoutDetails({ wName }) {
+function WorkoutDetails(props) {
   const { currentUser } = useSelector((state) => state);
   const [workouts] = useState([{ ...currentUser.workouts }]);
 
-     let workout = workouts.filter(
-       (workout) => workout[0].workoutName === wName
-     );
-     console.log(workout);
+  const myWorkouts = Object.values(workouts[0]);
+
+  const selectedWorkout = myWorkouts.filter((workout) => {
+    return workout.workoutName === props.wName;
+  });
+
+  // const thisWorkout = Object.values(selectedWorkout[0]);
+  // console.log(thisWorkout);
+
+    console.log(selectedWorkout);
+    const thisWorkout = Object.values(selectedWorkout[0]);
+    console.log(thisWorkout);
+    console.log(thisWorkout[3][0].day);
+    const workout = thisWorkout.map((workout) => {
+      return workout;
+    });
+    console.log(workout);
+    console.log(thisWorkout[3]);
+    const workoutDays = Object.values(thisWorkout[3]);
+    console.log(workoutDays);
+    console.log(workoutDays[1].day)
+
+    
+  const exerciseRows = workoutDays.map((index) => {
+    return index.exercises;
+  });
+
+  console.log(exerciseRows);
 
   return (
     <div className="container">
-      {workouts.map((workout, index) => (
-        <div key={index} className="shadow-lg p-3 mb-5 bg-white rounded">
-          <h3 className="text-center">{workout[index].routine[index].day}</h3>
+      {workoutDays.map((index) => (
+        <div className="shadow-lg p-3 mb-5 bg-white rounded">
+          <h3 className="text-center">{index.day}</h3>
           <table className="table table-dark">
             <thead>
               <tr>
@@ -25,31 +49,60 @@ function WorkoutDetails({ wName }) {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope="row">
-                  {workout[0].routine[0].exercises[0].exerciseName}
-                </th>
-                <td>{workout[0].routine[0].exercises[0].exerciseType}</td>
-                <td>{workout[0].routine[0].exercises[0].sets}</td>
-                <td>{workout[0].routine[0].exercises[0].reps}</td>
-              </tr>
-              <tr>
-                <th scope="row">{workout[0].routine[0].exercises[1].exerciseName}</th>
-                <td>{workout[0].routine[0].exercises[1].exerciseType}</td>
-                <td>{workout[0].routine[0].exercises[1].sets}</td>
-                <td>{workout[0].routine[0].exercises[1].reps}</td>
-              </tr>
-              <tr>
-                <th scope="row">{workout[0].routine[0].exercises[2].exerciseName}</th>
-                <td>{workout[0].routine[0].exercises[2].exerciseType}</td>
-                <td>{workout[0].routine[0].exercises[2].sets}</td>
-                <td>{workout[0].routine[0].exercises[2].reps}</td>
-              </tr>
+              {exerciseRows.map((exercise, index) => (
+                <tr>
+                  <th key={exercise._id} scope="row">
+                    {exercise.exerciseName}
+                  </th>
+                  <td>{exercise.exerciseType}</td>
+                  <td>{exercise.sets}</td>
+                  <td>{exercise.reps}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
       ))}
     </div>
+    // <div className="container">
+    //   {myWorkouts.map((workout, index) => (
+    //     <div key={index} className="shadow-lg p-3 mb-5 bg-white rounded">
+    //       <h3 className="text-center">placeholder</h3>
+    //       <table className="table table-dark">
+    //         <thead>
+    //           <tr>
+    //             <th scope="col">Exercise</th>
+    //             <th scope="col">Category</th>
+    //             <th scope="col">Sets</th>
+    //             <th scope="col">Reps</th>
+    //           </tr>
+    //         </thead>
+    //         <tbody>
+    //           <tr>
+    //             <th scope="row">
+    //               {workout[0].routine[0].exercises[0].exerciseName}
+    //             </th>
+    //             <td>{workout[0].routine[0].exercises[0].exerciseType}</td>
+    //             <td>{workout[0].routine[0].exercises[0].sets}</td>
+    //             <td>{workout[0].routine[0].exercises[0].reps}</td>
+    //           </tr>
+    //           <tr>
+    //             <th scope="row">{workout[0].routine[0].exercises[1].exerciseName}</th>
+    //             <td>{workout[0].routine[0].exercises[1].exerciseType}</td>
+    //             <td>{workout[0].routine[0].exercises[1].sets}</td>
+    //             <td>{workout[0].routine[0].exercises[1].reps}</td>
+    //           </tr>
+    //           <tr>
+    //             <th scope="row">{workout[0].routine[0].exercises[2].exerciseName}</th>
+    //             <td>{workout[0].routine[0].exercises[2].exerciseType}</td>
+    //             <td>{workout[0].routine[0].exercises[2].sets}</td>
+    //             <td>{workout[0].routine[0].exercises[2].reps}</td>
+    //           </tr>
+    //         </tbody>
+    //       </table>
+    //     </div>
+    //   ))}
+    // </div>
   );
 }
 
